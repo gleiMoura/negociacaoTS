58d8cc7358bbfabc6ad38b776833b3b700eb8744
gleiMoura <gle.bra.mou@gmail.com>
2022-10-08 13:08:14 -0300

    adiciona Typescript

---
+++ app/models/Negociacoes.ts
@@ -0,0 +1,27 @@
+export default class Negociacao {
+	_data: Date;
+	_quantidade: number;
+	_valor: number;
+
+	constructor(data: Date, quantidade: number, valor: number) {
+		this._data = data;
+		this._quantidade = quantidade;
+		this._valor = valor;
+	}
+
+	get data() {
+		return this._data;
+	}
+
+	get quantidade() {
+		return this._quantidade;
+	}
+
+	get valor() {
+		return this._valor;
+	}
+
+	get volume() {
+		return this.quantidade * this.valor;
+	}
+}

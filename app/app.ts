7742636ff03153f43327a99789bcccec4f2f1bdc
gleiMoura <gle.bra.mou@gmail.com>
2022-10-12 00:22:49 -0300

    cria uma classe de negociação que renderiza elementos HTML no DOM

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { negociacaoController } from "./controllers/negociacaoController.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 const controller = new negociacaoController;
 const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {

5deda73ef17e74762e3683c4409dfade37da47fd
gleiMoura <gle.bra.mou@gmail.com>
2022-10-12 16:42:43 -0300

    alteraçoes

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,6 +1,6 @@
 import { View } from "./view.js";
 
-export class mensagemView extends View {
+export class mensagemView extends View<string> {
 	template(model: string):string {
 		return `
 			<p class="alert alert-info">${model}</p>
--- app/views/view.ts
+++ app/views/view.ts
@@ -5,7 +5,7 @@ export abstract class View<T> {
 		this.element = document.querySelector(seletor);
 	}
 
-	update(model:T):void {
+	protected update(model:T):void {
 		const template = this.template(model);
 		this.element.innerHTML = template;
 	}
